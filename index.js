6821d475800c50161215f5c419bc40a562ace558
me4hit <nghiahuawei10101999@gmail.com>
2021-01-10 03:42:57 +0700

    test heroku

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = 3000 || process.env.PORT
 const puppeteer = require('puppeteer');
-
+const chromeOptions = {
+    headless: false,
+    defaultViewport: null,
+    args: [
+        "--no-sandbox"
+    ],
+};
 app.get('/',async (req, res) => {
-    const browser = await puppeteer.launch(   {
-        headless: true
-    });
+    
+    const browser = await puppeteer.launch(chromeOptions);
     const page = await browser.newPage();
     await page.goto(req.query.url);
     let img= await page.screenshot({ path: 'example.png' });
